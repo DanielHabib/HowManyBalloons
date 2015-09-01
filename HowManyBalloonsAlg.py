a6d22c4e0eafec6f6df4a252302c11adb9313204
demoleas21 <andrew.demoleas@gmail.com>
2015-09-01 13:09:55 -0400

    member variables give error

--- HowManyBalloonsAlg.py
+++ HowManyBalloonsAlg.py
@@ -3,12 +3,15 @@
 import math
 
 class BalloonCount(object):
+	grams = 453.593
+    liters_in_balloon = 14
     def __init__(self, weight):
         self.weight = weight
 
     def how_many_balloons(self):
-        balloons = self.weight * 453.593 / 14
+        balloons = self.weight * grams / liters_in_balloon
         return ("It would take %s balloons to lift %s lbs!" % (math.ceil(balloons), self.weight)) 
+
 
 weight = BalloonCount(int(input("Enter your weight ")))
 print (weight.how_many_balloons())
