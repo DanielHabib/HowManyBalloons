be2e22a9c867c35acdfb980156c0e5d319fa3635
demoleas21 <andrew.demoleas@gmail.com>
2015-08-31 21:54:39 -0400

    return statement edited

--- HowManyBalloonsAlg.py
+++ HowManyBalloonsAlg.py
@@ -8,8 +8,7 @@ class BalloonCount(object):
 
     def how_many_balloons(self):
         balloons = self.weight * 453.593 / 14
-        return math.ceil(balloons)
+        return ("It would take %s balloons to lift %s lbs!" % (math.ceil(balloons), self.weight)) 
 
 weight = BalloonCount(int(input("Enter your weight ")))
-number_of_balloons = weight.how_many_balloons()
-print("It would take %s balloons to lift %s lbs!" % (number_of_balloons, weight.weight))
+print (weight.how_many_balloons())
