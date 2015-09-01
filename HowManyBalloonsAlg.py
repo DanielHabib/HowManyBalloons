bb7ba0d5105e6ae05fb0b05a4c159c4a3eb2a4b6
Daniel Habib <d.g.habib7@gmail.com>
2015-09-01 13:19:27 -0400

    Update HowManyBalloonsAlg.py

--- HowManyBalloonsAlg.py
+++ HowManyBalloonsAlg.py
@@ -3,7 +3,7 @@
 import math
 
 class BalloonCount(object):
-	grams = 453.593
+	  grams = 453.593
     liters_in_balloon = 14
     def __init__(self, weight):
         self.weight = weight
@@ -12,6 +12,6 @@ class BalloonCount(object):
         balloons = self.weight * grams / liters_in_balloon
         return ("It would take %s balloons to lift %s lbs!" % (math.ceil(balloons), self.weight)) 
 
-
-weight = BalloonCount(int(input("Enter your weight ")))
-print (weight.how_many_balloons())
+if __name__=='__main__':
+    weight = BalloonCount(int(input("Enter your weight ")))
+    print (weight.how_many_balloons())
