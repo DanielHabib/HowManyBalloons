dff461897dd239e4b1e688e6e8b9987f78fbccc6
demoleas21 <andrew.demoleas@gmail.com>
2015-09-03 22:42:02 -0400

    algorithm attempt

---
+++ HowManyBalloonsAlg.js
@@ -0,0 +1,38 @@
+/* How Many Balloons Algorithm */
+import React // { PropTypes } from 'react';
+//import withViewport from '../../decorators/withViewport';
+//import withStyles from '../../decorators/withStyles';
+
+//Test Function (didnt work)
+/*var GroceryList = React.createClass({
+  handleClick: function(i) {
+    console.log('You clicked: ' + this.props.items[i]);
+  },
+
+  render: function() {
+    return (
+      <div>
+        {this.props.items.map(function(item, i) {
+          return (
+            <div onClick={this.handleClick.bind(this, i)} key={i}>{item}</div>
+          );
+        }, this)}
+      </div>
+    );
+  }
+}); 
+
+React.render(
+  <GroceryList items={['Apple', 'Banana', 'Cranberry']} />, mountNode
+); */
+
+//basic function
+var BalloonCount = React.createClass({
+    render: function(){
+        var balloons = weight * 453.593 / 14;
+    }
+});
+
+//calling the function, havnt even gotten this far
+weight = 150;
+balloon_count(weight);
